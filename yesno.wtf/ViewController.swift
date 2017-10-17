cd25d23c51182336f9ac5e2f92f71824e78d2134
Yakov Kadosh <kobi.kadosh@gmail.com>
2017-10-17 22:44:32 +0300

    Initial Commit

---
+++ yesno.wtf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yesno.wtf
+//
+//  Created by Yakov Kadosh on 17/10/17.
+//  Copyright © 2017 Kobi Kadosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
